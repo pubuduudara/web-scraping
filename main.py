52122e7c74fa4427bd2a7d58a66ff39b99f0c7cd
Pubudu Premathilaka <pubuduudara7@gmail.com>
2024-05-30 08:09:35 +0530

    code refactor

--- main.py
+++ main.py
@@ -44,9 +44,9 @@ def run():
                 all = []
                 while True:
                     results_html = driver.page_source
-                    if business_type == const.GENERAL_CONTRACT:
+                    if business_type == const.CONST_GENERAL_CONTRACT:
                         active_business = extract_general_contract_active_businesses(results_html)
-                    elif business_type == const.ELECTRICAL_FIRM:
+                    elif business_type == const.CONST_ELECTRICAL_FIRM:
                         active_business = extract_electrical_firm_active_businesses(results_html)
                     else:
                         print('Not a matching business business_type')
@@ -115,54 +115,104 @@ def process_each_business(driver, active_businesses, business_type):
         business_page_source = driver.page_source
         soup = BeautifulSoup(business_page_source, 'html.parser')
 
-        if business_type == const.ELECTRICAL_FIRM:
-            # Extract Office Address
-            address_tag = soup.find('td', string=lambda text: 'Office Address' in text if text else False)
-            if address_tag:
-                address = address_tag.find_next_sibling('td').get_text(strip=True)
-            else:
-                address = const.NA
-            # Extract phone
-            business_phone_tag = soup.find('td', string=lambda text: 'Business Phone' in text if text else False)
-            if business_phone_tag:
-                business_phone_value = business_phone_tag.find_next_sibling('td').get_text(strip=True)
-            else:
-                business_phone_value = const.NA
-
-            # business name
-            business_name_tag = soup.find('b', string=lambda text: 'Business 1' in text if text else False)
-            if business_name_tag:
-                business_name = business_name_tag.parent.get_text(strip=True).split(':')[-1].strip()
-            else:
-                business_name = const.NA
+        if business_type == const.CONST_ELECTRICAL_FIRM:
+            contact_details = extract_electrical_firm_contact_details(soup)
 
             # Find the correct table
             table = soup.find_all('table')[6]
-            if len(table.find_all('tr')) <= 1:
-                print("Table has no entries")
-                # TODO: check with the client because this record will not be added to the database
-                continue
-            else:
-                # Define variables to hold the extracted values
-                licensee_list = []
-                # Iterate through the table rows
-                for row in table.find_all('tr')[1:]:
-                    columns = row.find_all('td')
-                    relationship = columns[2].get_text(strip=True)
-                    if relationship == 'RESPONSIBLE REP':
-                        licensee = columns[0].get_text(strip=True)
-                        license_number = columns[1].get_text(strip=True)
-                        expiration_date = columns[3].get_text(strip=True)
-                        status = columns[4].get_text(strip=True)
-                        licensee_list.append([licensee, license_number, expiration_date, status])
 
-                print(licensee_list)
+            licensee_list = extract_electrical_firm_licensee_details(table)  # TODO: this can be empty
 
             insurance_list = extract_electrical_firm_insurance_details(soup.find_all('table')[4])
             print(insurance_list)
 
 
+def extract_electrical_firm_contact_details(soup):
+    """
+        Extracts insurance details for an electrical firm from the given BeautifulSoup object.
+
+        Args:
+            soup (BeautifulSoup): A BeautifulSoup object containing the parsed HTML of the page.
+
+        Returns:
+            dict: A dictionary containing the extracted details with keys:
+                - CONST_BUSINESS_NAME: The name of the business.
+                - CONST_BUSINESS_OFFICE_ADDRESS: The office address of the business.
+                - CONST_BUSINESS_PHONE_NUMBER: The business phone number.
+        """
+    # Extract Office Address
+    address_tag = soup.find('td', string=lambda text: 'Office Address' in text if text else False)
+    if address_tag:
+        address = address_tag.find_next_sibling('td').get_text(strip=True)
+    else:
+        address = const.CONST_NA
+    # Extract phone
+    business_phone_tag = soup.find('td', string=lambda text: 'Business Phone' in text if text else False)
+    if business_phone_tag:
+        business_phone_value = business_phone_tag.find_next_sibling('td').get_text(strip=True)
+    else:
+        business_phone_value = const.CONST_NA
+
+    # business name
+    business_name_tag = soup.find('b', string=lambda text: 'Business 1' in text if text else False)
+    if business_name_tag:
+        business_name = business_name_tag.parent.get_text(strip=True).split(':')[-1].strip()
+    else:
+        business_name = const.CONST_NA
+
+    return {const.CONST_BUSINESS_NAME: business_name, const.CONST_BUSINESS_OFFICE_ADDRESS: address,
+            const.CONST_BUSINESS_PHONE_NUMBER: business_phone_value}
+
+
+def extract_electrical_firm_licensee_details(table):
+    """
+        Extracts licensee details for an electrical firm from the given HTML table.
+
+        Args:
+            table (BeautifulSoup): A BeautifulSoup object containing the parsed HTML table.
+
+        Returns:
+            list: A list of lists, each containing the following details for each 'RESPONSIBLE REP':
+                - Licensee name
+                - License number
+                - Expiration date
+                - Status
+        """
+    if len(table.find_all('tr')) <= 1:
+        print("Table has no licensee entries")
+        return []
+    else:
+        # Define variables to hold the extracted values
+        licensee_list = []
+        # Iterate through the table rows
+        for row in table.find_all('tr')[1:]:
+            columns = row.find_all('td')
+            relationship = columns[2].get_text(strip=True)
+            if relationship == 'RESPONSIBLE REP':
+                licensee = columns[0].get_text(strip=True)
+                license_number = columns[1].get_text(strip=True)
+                expiration_date = columns[3].get_text(strip=True)
+                status = columns[4].get_text(strip=True)
+                licensee_list.append([licensee, license_number, expiration_date, status])
+
+        return licensee_list
+
+
 def extract_electrical_firm_insurance_details(table):
+    """
+        Extracts insurance details for an electrical firm from the given HTML table.a
+
+        Args:
+            table (BeautifulSoup): A BeautifulSoup object containing the parsed HTML table.
+
+        Returns:
+            list: A list of lists, each containing the following details for each insurance entry:
+                - Insurance type
+                - Policy number
+                - Required (yes/no)
+                - Insurance company
+                - Expiration date
+        """
     insurance_list = []
 
     # Iterate through the table rows
